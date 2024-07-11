cd99f8d70729f639b9382cf6e1027b83f7d27aaa
Lelehjustino <lehjustinowehbe@gmail.com>
2024-07-11 19:15:41 -0300

    Problemas nos dois primeiros botões arrumados

--- products.js
+++ products.js
@@ -1,7 +1,7 @@
 const menuOptions = [
     { name: 'X-Salada', price: 23, vegan: false, src: './img/x-salada.jpeg' },
-    { name: 'X-Bacon', price: 25, vegan: false, src: './img/x-bacon.png' },
-    { name: 'X-Bacon Egg', price: 30, vegan: false, src: './img/bacon-egg.png' },
+    { name: 'X-Bacon', price: 25, vegan: false, src: './img/x-bancon.png' },
+    { name: 'X-Bacon Egg', price: 30, vegan: false, src: './img/bancon-egg.png' },
     { name: 'Monstruoso', price: 45, vegan: false, src: './img/monstruoso.png' },
     { name: 'X-Vegan', price: 25, vegan: true, src: './img/x-vegan.png' },
     { name: 'Monstruoso Vegano', price: 40, vegan: true, src: './img/monstruoso-vegan.png' }
